d24d9a5bdaba9ddecde0883b5f9c8981dab1d0df
Slayer <slayerpro27@gmail.com>
2024-09-22 18:29:57 +0300

    Fixed proxy linking issue for .session files, got from elsewhere Improved session locking

--- bot/core/tapper.py
+++ bot/core/tapper.py
@@ -57,59 +57,41 @@ class Tapper:
 
         self.tg_client.set_proxy(proxy_dict)
 
-        try:
-            if not self.tg_client.is_connected():
-                try:
-                    self.lock.acquire()
-                    await self.tg_client.start()
-                except (UnauthorizedError, AuthKeyUnregisteredError):
-                    raise InvalidSession(self.session_name)
-                except (UserDeactivatedError, UserDeactivatedBanError, PhoneNumberBannedError):
-                    raise InvalidSession(f"{self.session_name}: User is banned")
+        tg_web_data = None
+        with self.lock:
+            async with self.tg_client as client:
+                while True:
+                    try:
+                        resolve_result = await client(contacts.ResolveUsernameRequest(username='DiamoreCryptoBot'))
+                        peer = InputPeerUser(user_id=resolve_result.peer.user_id,
+                                             access_hash=resolve_result.users[0].access_hash)
+                        break
+                    except FloodWaitError as fl:
+                        fls = fl.seconds
 
-            while True:
-                try:
-                    resolve_result = await self.tg_client(contacts.ResolveUsernameRequest(username='DiamoreCryptoBot'))
-                    peer = InputPeerUser(user_id=resolve_result.peer.user_id,
-                                         access_hash=resolve_result.users[0].access_hash)
-                    break
-                except FloodWaitError as fl:
-                    fls = fl.seconds
+                        logger.warning(self.log_message(f"FloodWait {fl}"))
+                        logger.info(self.log_message(f"Sleep {fls}s"))
+                        await asyncio.sleep(fls + 3)
 
-                    logger.warning(self.log_message(f"FloodWait {fl}"))
-                    logger.info(self.log_message(f"Sleep {fls}s"))
-                    await asyncio.sleep(fls + 3)
+                start_param = settings.REF_ID if random.randint(0, 100) <= 85 else "525256526"
 
-            start_param = settings.REF_ID if random.randint(0, 100) <= 85 else "525256526"
+                input_user = InputUser(user_id=resolve_result.peer.user_id,
+                                       access_hash=resolve_result.users[0].access_hash)
+                input_bot_app = InputBotAppShortName(bot_id=input_user, short_name="app")
 
-            input_user = InputUser(user_id=resolve_result.peer.user_id, access_hash=resolve_result.users[0].access_hash)
-            input_bot_app = InputBotAppShortName(bot_id=input_user, short_name="app")
+                web_view = await self.tg_client(messages.RequestAppWebViewRequest(
+                    peer=peer,
+                    app=input_bot_app,
+                    platform='android',
+                    write_allowed=True,
+                    start_param=start_param
+                ))
 
-            web_view = await self.tg_client(messages.RequestAppWebViewRequest(
-                peer=peer,
-                app=input_bot_app,
-                platform='android',
-                write_allowed=True,
-                start_param=start_param
-            ))
+                auth_url = web_view.url
+                tg_web_data = unquote(
+                    string=auth_url.split('tgWebAppData=', maxsplit=1)[1].split('&tgWebAppVersion', maxsplit=1)[0])
 
-            auth_url = web_view.url
-            tg_web_data = unquote(
-                string=auth_url.split('tgWebAppData=', maxsplit=1)[1].split('&tgWebAppVersion', maxsplit=1)[0])
-
-            if self.tg_client.is_connected():
-                await self.tg_client.disconnect()
-                if self.lock.acquired:
-                    self.lock.release()
-
-            return tg_web_data
-
-        except InvalidSession as error:
-            return None
-
-        except Exception as error:
-            log_error(self.log_message(f"Unknown error during Authorization: {error}"))
-            return None
+        return tg_web_data
 
     async def user(self, http_client: aiohttp.ClientSession):
         try:
@@ -254,10 +236,11 @@ class Tapper:
                 if balance > price:
                     status = await self.do_upgrade(http_client=http_client, type=upgrade_type)
                     if status:
-                        logger.success( self.log_message(f'Successfully upgraded {log_message}, level - {level + 1}, '
-                                                         f'balance - {balance - price}'))
+                        logger.success(self.log_message(f'Successfully upgraded {log_message}, level - {level + 1}, '
+                                                        f'balance - {balance - price}'))
                     else:
-                        logger.warning(self.log_message(f'Something wrong in upgrade. Balance: {balance}. Upgrade price: {price}'))
+                        logger.warning(
+                            self.log_message(f'Something wrong in upgrade. Balance: {balance}. Upgrade price: {price}'))
                         break
                 else:
                     logger.info(self.log_message(f'Not enough money to upgrade {log_message}'))
@@ -373,20 +356,21 @@ class Tapper:
                             logger.success(self.log_message(f'Watched ad to skip game cooldown'))
                             status, clicks = await self.sync_clicks(http_client=http_client)
                             user = await self.user(http_client=http_client)
-                            logger.success(self.log_message(f'Played game, got - {clicks} diamonds, balance - {int(float(user["balance"]))}'))
+                            logger.success(self.log_message(
+                                f'Played game, got - {clicks} diamonds, balance - {int(float(user["balance"]))}'))
                         ads_count -= 1
 
                 if settings.AUTO_UPGRADE_REDUCE_COOLDOWN:
                     await self.upgrade_to_level(http_client, "tapCoolDown", "AUTO_UPGRADE_REDUCE_COOLDOWN_LEVEL",
-                                                   "game cooldown", "game cooldown")
+                                                "game cooldown", "game cooldown")
 
                 if settings.AUTO_UPGRADE_CLICKING_POWER:
                     await self.upgrade_to_level(http_client, "tapPower", "AUTO_UPGRADE_CLICKING_POWER_LEVEL",
-                                                   "game tap power", "game tap power")
+                                                "game tap power", "game tap power")
 
                 if settings.AUTO_UPGRADE_TIMER:
                     await self.upgrade_to_level(http_client, "tapDuration", "AUTO_UPGRADE_TIMER_LEVEL",
-                                                   "game duration", "game duration")
+                                                "game duration", "game duration")
 
                 if next_tap_delay is None or next_tap_delay.seconds > 3600:
                     sleep_time = random.randint(3500, 3600)
