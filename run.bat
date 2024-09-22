ce8a4e08b170b5d9f38f10193d3efe0397691040
Slayer <slayerpro27@gmail.com>
2024-09-23 00:20:27 +0300

    Updated script

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+title Diamore
+set firstRun=true
 
 if not exist venv (
     echo Creating virtual environment...
@@ -31,9 +33,15 @@ if not exist .env (
 
 git fetch
 git pull
-echo Starting the bot...
-python main.py
 
-echo done
-echo PLEASE EDIT .ENV FILE
-pause
+echo Starting the bot...
+:loop
+if "%firstRun%"=="true" (
+    python main.py
+    set firstRun=false
+) else (
+    python main.py -a 1
+)
+echo Restarting the program in 10 seconds...
+timeout /t 10 /nobreak >nul
+goto :loop
